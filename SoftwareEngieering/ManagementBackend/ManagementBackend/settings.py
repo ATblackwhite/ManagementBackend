eedd764038f38be404642c02402dcfddc3f5e9b3
ATblackwhite <78542416+ATblackwhite@users.noreply.github.com>
2023-04-29 22:48:32 +0800

    Update settings.py

--- SoftwareEngieering/ManagementBackend/ManagementBackend/settings.py
+++ SoftwareEngieering/ManagementBackend/ManagementBackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b!_(eh3g3u)p+e_n7!(%z79p)9qk!(4-wp7jr0$qii(^3y7=c#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
