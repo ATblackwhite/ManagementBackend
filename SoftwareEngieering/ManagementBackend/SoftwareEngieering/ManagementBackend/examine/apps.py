0e975f528c5d5bd892eff7babf79fdb2b5b67c0a
ATblackwhite <1621864188@qq.com>
2023-05-23 23:06:44 +0800

    删除重复内容

--- SoftwareEngieering/ManagementBackend/SoftwareEngieering/ManagementBackend/examine/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExamineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'examine'
--- SoftwareEngieering/ManagementBackend/SoftwareEngieering/ManagementBackend/examine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from . import views
-from django.views.static import serve
-urlpatterns = [
-    path('list/', views.examine_list),
-    path('accept/', views.accept_op),
-    path('reject/', views.reject_op),
-]
